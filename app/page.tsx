1ea92d0753e9805594876609feabea357194dccd
Abraham Vega <abraham@localhost.localdomain>
2024-06-07 08:31:30 -0500

    finished tournament domain

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
-export default function App({ children }: { children: React.ReactNode }) {
-  return <main className='flex h-full w-full'>{children}</main>;
+export default function FrontPage() {
+  return (
+    <main className='flex h-full w-full items-center justify-center'>
+      <h1 className='text-3xl font-bold'> Bienvenido</h1>
+    </main>
+  );
 }
