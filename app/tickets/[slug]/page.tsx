642b7ec3ba83366e4b03feb94a6ee9d2e35181d3
Abraham Vega <abraham@localhost.localdomain>
2025-04-08 03:56:37 -0500

    tickets added

--- app/tickets/[slug]/page.tsx
+++ app/tickets/[slug]/page.tsx
@@ -6,15 +6,13 @@ export default async function Tickets({
 }: {
   readonly params: { readonly slug?: string };
 }) {
-  const resolvedParams = await params;
-
-  if (!resolvedParams?.slug) {
+  if (!params?.slug) {
     redirect('/');
   }
 
   return (
     <div className='flex h-full w-full flex-grow flex-col items-start justify-center py-5'>
-      <TicketResume ticketId={resolvedParams.slug} />
+      <TicketResume ticketId={params.slug} />
     </div>
   );
 }
--- app/tournaments/[slug]/page.tsx
+++ app/tournaments/[slug]/page.tsx
@@ -6,15 +6,13 @@ export default async function Tournament({
 }: {
   readonly params: { readonly slug?: string };
 }) {
-  const resolvedParams = await params;
-
-  if (!resolvedParams?.slug) {
+  if (!params?.slug) {
     redirect('/');
   }
 
   return (
     <div className='flex h-full w-full flex-grow flex-col items-start justify-center py-5'>
-      <TournamentResume tournamentId={resolvedParams.slug} />
+      <TournamentResume tournamentId={params.slug} />
     </div>
   );
 }
